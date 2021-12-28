6b4b642bfcb531bb77475b8c89cf53f89d4f4ea0
bilkiss <bilkiss.dulloo@gmail.com>
2021-12-28 11:08:13 +0400

    Completed product listing

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../../services/storage.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  productList$ = this.storage.watch('products');
+
+  constructor(
+    public storage: StorageService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  removeStoreProduct(name: string): void{
+    let list = this.storage.get('products');
+    if (!list) {
+      list = [];
+    }
+    list = list.filter( (x: any) => x.name != name);
+    this.storage.set('products', list);
   }
 
 }
