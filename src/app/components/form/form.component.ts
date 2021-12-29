19d03ae1891a82252448055be3d303e5d19dfc85
bilkiss <bilkiss.dulloo@gmail.com>
2021-12-29 11:00:11 +0400

    Completed total

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -15,6 +15,8 @@ export class FormComponent implements OnInit {
     price: 0
   };
   storageProductList: any = [];
+  productNameError = false;
+  productPriceError = false;
 
   constructor(
     public storage: StorageService
@@ -35,11 +37,13 @@ export class FormComponent implements OnInit {
       // console.log('Object.keys(key):' , Object.keys(key));
       this.productNames.push.apply(this.productNames, Object.keys(key));
     });
-    console.log('this.productNames:' , this.productNames);
+    // console.log('this.productNames:' , this.productNames);
   }
 
   onChangeProduct(event: any): void{
     const value = event.target.value
+    this.productNameError = (!value) ? true : false;
+    this.productPriceError = (parseInt(this.currentProduct.price) < 1) ? true : false;
 
     this.products.map( prod => {
       if (Object.keys(prod)[0].includes(value)){
@@ -48,18 +52,28 @@ export class FormComponent implements OnInit {
     });
 
     // this.currentProduct.price = this.products.filter(o => Object.keys(o).includes( value ) )[0][value];    
-    console.log('onChangeProduct currentProduct.price: ', this.currentProduct.price);
+    // console.log('onChangeProduct currentProduct.price: ', this.currentProduct.price);
 
   }
 
-  addProduct(): void{
-    console.log('addProduct currentProduct: ', this.currentProduct);
-    this.storageProductList = this.storage.get('products');
-    if (!this.storageProductList) {
-      this.storageProductList = [];
+  addProduct(): void{    
+    // console.log('addProduct currentProduct: ', this.currentProduct);
+    if (!this.currentProduct.name || this.currentProduct.price == 0) {
+      this.productNameError = this.currentProduct.name ? false : true;
+      this.productPriceError = (parseInt(this.currentProduct.price) < 1) ? true : false;
+    } else {
+      this.productNameError = false;
+      this.productPriceError = false;
+      this.currentProduct.price = parseInt(this.currentProduct.price); 
+      this.storageProductList = this.storage.get('products');
+      if (!this.storageProductList) {
+        this.storageProductList = [];
+      }
+      this.storageProductList.push({...this.currentProduct});
+      this.storage.set('products', this.storageProductList);
     }
-    this.storageProductList.push({...this.currentProduct});
-    this.storage.set('products', this.storageProductList);
+
+
   }
 
 
--- src/app/components/total/total.component.ts
+++ src/app/components/total/total.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../../services/storage.service';
 
 @Component({
   selector: 'app-total',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TotalComponent implements OnInit {
 
-  constructor() { }
+  displayProductData: any = {
+    subtotal: 0,
+    discount: 0,
+    vat: 0,
+    total: 0
+  };
+
+  constructor(
+    public storage: StorageService
+  ) { }
 
   ngOnInit(): void {
+    this.storage.watch('products')?.subscribe( res => {
+      // console.log('watch res: ', res);
+      const prices = res.map( (x: any) => x.price);
+
+      this.displayProductData.subtotal = (prices.length > 1) ? 
+      prices.reduce( (a:number, b:number) => a+b, 0 ): prices[0];
+
+      this.displayProductData.discount = 0.05 * this.displayProductData.subtotal;
+      this.displayProductData.vat = 0.15 * this.displayProductData.subtotal;
+      this.displayProductData.total = this.displayProductData.subtotal - this.displayProductData.discount + this.displayProductData.vat;
+
+      // console.log('onInit displayProductData: ', this.displayProductData);
+    });
   }
 
 }
